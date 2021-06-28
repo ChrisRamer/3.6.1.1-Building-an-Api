03f74ff0a2f7ad7a7171bdf5d10b9be477d5bb16
Chris Ramer <chrisramer.97@gmail.com>
2021-06-28 09:32:08 -0700

    Run on multiple ports

--- PokemonApi/Program.cs
+++ PokemonApi/Program.cs
@@ -19,6 +19,7 @@ namespace PokemonApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5001/", "http://localhost:5004/");
     }
 }
