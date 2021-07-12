d3fa2d9262b1d3753903f6372e472abb3fd76a73
Chris Ramer <chrisramer.97@gmail.com>
2021-07-12 09:55:33 -0700

    Add remaining routes

--- PokemonApi/Controllers/PokemonController.cs
+++ PokemonApi/Controllers/PokemonController.cs
@@ -83,11 +83,56 @@ namespace PokemonApi.Controllers
 			_db = db;
 		}
 
+		// GET api/pokemon
+		[HttpGet]
+		public ActionResult<IEnumerable<Pokemon>> Get(int index, string name)
+		{
+			var query = _db.Pokemon.AsQueryable();
+
+			if (index > 0)
+			{
+				query = query.Where(entry => entry.PokemonId == index);
+			}
+
+			if (name != null)
+			{
+				query = query.Where(entry => entry.Name == name);
+			}
+
+			return query.ToList();
+		}
+
+		// Post api/pokemon/5
+		[HttpPost]
+		public void Post([FromBody] Pokemon pokemon)
+		{
+			_db.Pokemon.Add(pokemon);
+			_db.SaveChanges();
+		}
+
 		// GET api/pokemon/5
 		[HttpGet("{id}")]
 		public ActionResult<Pokemon> GetActionResult(int id)
 		{
 			return _db.Pokemon.FirstOrDefault(entry => entry.PokemonId == id);
+		}
+
+		// PUT api/pokemon/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] Pokemon pokemon)
+		{
+			pokemon.PokemonId = id;
+			_db.Entry(pokemon).State = EntityState.Modified;
+			_db.SaveChanges();
+		}
+
+		// DELETE api/pokemon/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+			Pokemon pokemonToDelete = _db.Pokemon.FirstOrDefault(entry => entry.PokemonId == id);
+			_db.Pokemon.Remove(pokemonToDelete);
+			_db.SaveChanges();
 		}
 	}
 }
